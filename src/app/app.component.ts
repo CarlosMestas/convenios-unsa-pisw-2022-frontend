c8a48c9dccd205b7b97e1585280ca60d851e6e0b
kticllahuanaco <kticllahuanaco@unsa.edu.pe>
2022-06-03 12:01:31 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'convenios-unsa-pisw-2022-frontend';
+}
