b8ab62d2673b1786c783e7f7b0d41ded7310f53a
kelvin <kticllahuanaco@unsa.edu.pe>
2022-08-24 22:22:42 -0500

    fix service method isn't called

--- src/app/ngrx/effects/index.effects.ts
+++ src/app/ngrx/effects/index.effects.ts
@@ -2,9 +2,11 @@ import { ProfileEffect } from './profile/profile.effects';
 import { UserAuthEffect } from './auth/user-auth.effects';
 import { ConvocationEffect } from './convocation/convocation.effect';
 import { DocumentEffect } from './convocation/document.effect';
+import { AdminAuthEffect } from './admin/admin-auth.effects';
 export const effectsOF = [
   UserAuthEffect,
   ProfileEffect,
   ConvocationEffect,
-  DocumentEffect
+  DocumentEffect,
+  AdminAuthEffect
 ]
