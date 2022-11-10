fdf19d00285a42e053872aebeb72e7973ee391d8
KELVIN TICLLAHUANACO <sistemas.alfanevado@gmail.com>
2022-11-10 08:03:25 -0500

    production path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url:'https://api-oficina-convenios.herokuapp.com/api/'//en un futuro esta URL debería ser la de producción
-  // url: 'https://convenios-unsa-pisw-2022-0.herokuapp.com/api/'
+  // url:'https://api-oficina-convenios.herokuapp.com/api/'//en un futuro esta URL debería ser la de producción
+  url: 'https://convenios-unsa-pisw-2022-0.herokuapp.com/api/'
 };
